e55d25dfd6918262da34e993d98846deb21bcce5
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-10-09 20:16:44 +0300

    Initial Commit

---
+++ Quizz.Animal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizz.Animal
+//
+//  Created by Igor Shelginskiy on 09/10/2018.
+//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
